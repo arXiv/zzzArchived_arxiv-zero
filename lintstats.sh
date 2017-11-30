676598fe4129373334ec506cc9cc26ff84632419
erickpeirson <brp53@cornell.edu>
2017-11-30 15:57:53 -0500

    fixes for lintstats.sh

--- lintstats.sh
+++ lintstats.sh
@@ -1,21 +1,9 @@
+if [ -z ${MIN_SCORE} ]; then MIN_SCORE="9"; fi
 PYLINT_SCORE=$( pylint --disable=$PYLINT_IGNORE -f parseable zero | tail -2 | grep -Eo '[0-9\.]+/10' | tail -1 | sed s/\\/10// )
-PYLINT_PASS=$(echo $SCORE">="$MIN_SCORE | bc -l)
+PYLINT_PASS=$(echo $PYLINT_SCORE">="$MIN_SCORE | bc -l)
 
-if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ]    # This is not a pull request.
-then
-    SHA=$TRAVIS_COMMIT
-else
-    SHA=$TRAVIS_PULL_REQUEST_SHA
-fi
-
-if [ $PYLINT_PASS ]
-then
-    PYLINT_STATE="success"
-    echo "pylint passed with score "$PYLINT_SCORE" for sha "$SHA
-else
-    PYLINT_STATE="failure"
-    echo "pylint failed with score "$PYLINT_SCORE" for sha "$SHA
-fi
+if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
+if [ "$PYLINT_PASS" ]; then PYLINT_STATE="success" &&  echo "pylint passed with score "$PYLINT_SCORE" for sha "$SHA; else PYLINT_STATE="failure" &&  echo "pylint failed with score "$PYLINT_SCORE" for sha "$SHA; fi
 
 curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$PYLINT_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "'$PYLINT_SCORE'/10", "context": "code-quality/pylint"}' \
@@ -24,14 +12,8 @@ curl -u $USERNAME:$GITHUB_TOKEN \
 
 
 mypy -p zero --ignore-missing-imports
-if [ "$?" ]
-then
-    MYPY_STATE="success"
-    echo "mypy passed"
-else
-    MYPY_STATE="failure"
-    echo "mypy failed"
-fi
+MYPY_STATUS=$?
+if [ $MYPY_STATUS -ne 0 ]; then MYPY_STATE="failure" && echo "mypy failed"; else MYPY_STATE="success" &&  echo "mypy passed"; fi
 
 curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$MYPY_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "code-quality/mypy"}' \

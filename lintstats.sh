d4dac15a38024d8199a21f1a7f7680b394b3f1da
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-01-05 15:06:33 -0500

    removing PYLINT_IGNORE from vars

--- lintstats.sh
+++ lintstats.sh
@@ -1,7 +1,6 @@
-
 # Check pylint status
 if [ -z ${MIN_SCORE} ]; then MIN_SCORE="9"; fi
-PYLINT_SCORE=$( pylint --disable=$PYLINT_IGNORE -f parseable zero | tail -2 | grep -Eo '[0-9\.]+/10' | tail -1 | sed s/\\/10// )
+PYLINT_SCORE=$( pylint zero | tail -2 | grep -Eo '[0-9\.]+/10' | tail -1 | sed s/\\/10// )
 PYLINT_PASS=$(echo $PYLINT_SCORE">="$MIN_SCORE | bc -l)
 
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
@@ -9,15 +8,6 @@ if [ "$PYLINT_PASS" ]; then PYLINT_STATE="success" &&  echo "pylint passed with 
 
 curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$PYLINT_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "'$PYLINT_SCORE'/10", "context": "code-quality/pylint"}' \
-    -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
-    > /dev/null 2>&1
-
-# Check pylintrc status
-diff .pylintrc <(pylint --disable=$PYLINT_IGNORE -f parseable --generate-rcfile)
-PYLINTRC_STATUS=$?
-if [ $PYLINTRC_STATUS -ne 0 ]; then PYLINTRC_STATE="failure" && echo "pylintrc not updated or PYLINT_IGNORE not updated"; else PYLINTRC_STATE="success" &&  echo "pylintrc up-to-date"; fi
-curl -u $USERNAME:$GITHUB_TOKEN \
-    -d '{"state": "'$PYLINTRC_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "code-quality/pylintrc"}' \
     -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
     > /dev/null 2>&1
 

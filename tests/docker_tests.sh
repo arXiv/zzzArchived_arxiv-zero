2cb1f280dadf9725f371abcc6f0edc07f3426137
erickpeirson <brp53@cornell.edu>
2018-01-19 12:10:51 -0500

    cleanup

--- tests/docker_tests.sh
+++ tests/docker_tests.sh
@@ -4,7 +4,7 @@
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
 
 # Check that zero builds
-docker build -t arxiv-zero:latest .
+docker build -t arxiv-zero:latest ../
 DOCKER_BUILD_ZERO_STATUS=$?
 if [ $DOCKER_BUILD_ZERO_STATUS -ne 0 ]; then
     DOCKER_BUILD_ZERO_STATE="failure" && echo "docker build zero failed";
@@ -15,7 +15,6 @@ curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$DOCKER_BUILD_ZERO_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "docker/build"}' \
     -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
     > /dev/null 2>&1
-
 
 # Check that zero runs
 docker run -d -p 8000:8000 --name arxiv-zero-test arxiv-zero
@@ -42,6 +41,5 @@ curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$CURL_TEST_ZERO_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "curl/basic"}' \
     -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
     > /dev/null 2>&1
-
 
 docker rm -f arxiv-zero-test

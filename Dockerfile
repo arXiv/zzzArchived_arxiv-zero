db3f31a93400f69e96d93f94322b5655a4309749
erickpeirson <brp53@cornell.edu>
2018-02-08 12:58:27 -0500

    updated dockerfile to use pipenv successfully

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,14 @@
 
 FROM arxiv/base:latest
 
-#For local testing:
-# FROM arxiv-base:latest
-
 WORKDIR /opt/arxiv/
 
-ADD requirements.txt Pipfile Pipfile.lock /opt/arxiv/
-RUN pip install -U pip && \
-  pip install -r /opt/arxiv/requirements.txt && \
-  pipenv install
+RUN yum install -y which
+ADD Pipfile Pipfile.lock /opt/arxiv/
+RUN pip install -U pip pipenv
+ENV LC_ALL en_US.utf-8
+ENV LANG en_US.utf-8
+RUN pipenv install
 
 ENV PATH "/opt/arxiv:${PATH}"
 
--- tests/docker_tests.sh
+++ tests/docker_tests.sh
@@ -4,7 +4,8 @@
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
 
 # Check that zero builds
-docker build $TRAVIS_BUILD_DIR -t arxiv-zero:latest -f $TRAVIS_BUILD_DIR/Dockerfile
+echo "Build arxiv/zero in $TRAVIS_BUILD_DIR";
+docker build $TRAVIS_BUILD_DIR -t arxiv/zero -f $TRAVIS_BUILD_DIR/Dockerfile
 DOCKER_BUILD_ZERO_STATUS=$?
 if [ $DOCKER_BUILD_ZERO_STATUS -ne 0 ]; then
     DOCKER_BUILD_ZERO_STATE="failure" && echo "docker build zero failed";
@@ -17,7 +18,7 @@ curl -u $USERNAME:$GITHUB_TOKEN \
     > /dev/null 2>&1
 
 # Check that zero runs
-docker run -d -p 8000:8000 --name arxiv-zero-test arxiv-zero
+docker run -d -p 8000:8000 --name arxiv-zero-test arxiv/zero
 DOCKER_RUN_ZERO_STATUS=$?
 if [ $DOCKER_RUN_ZERO_STATUS -ne 0 ]; then
     DOCKER_RUN_ZERO_STATE="failure" && echo "docker run zero failed";

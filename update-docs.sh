9e1ba29b8d0ac83d9fc00bc33874f9644145b716
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-08-23 11:47:38 -0400

    initial doc update script

---
+++ update-docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+REPO=cul-it/arxiv-zero
+SRCDOCS=`pwd`/docs/build/html
+echo $SRCDOCS
+
+cd `pwd`/docs
+make html SPHINXDOCS=$(pipenv --venv)/bin/sphinx-build
+
+cd $SRCDOCS
+MSG="Adding gh-pages docs for `git log -1 --pretty=short --abbrev-commit`"
+
+TMPREPO=/tmp/docs/topic-explorer/
+rm -rf $TMPREPO
+mkdir -p -m 0755 $TMPREPO
+echo $MSG
+
+git clone git@github.com:$REPO.git $TMPREPO
+cd $TMPREPO
+git checkout gh-pages  ###gh-pages has previously one off been set to be nothing but html
+cp -r $SRCDOCS/* $TMPREPO
+git add -A
+git commit -m "$MSG" ## && git push origin gh-pages
